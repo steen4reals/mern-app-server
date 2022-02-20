e20cb5d417375c34b209cf3a381aea34f7852c78
Christina Ma <74700240+steen4reals@users.noreply.github.com>
2022-02-20 20:40:04 +0000

    Add files via upload

---
+++ db/conn.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require("mongodb");
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+var _db;
+
+module.exports = {
+    connectToServer: function(callback){
+        client.connect(function(err, db){
+            
+            if(db)
+            {
+                _db=db.db("myFirstDatabase");
+                console.log("successfully conected to MongoDB");
+            }
+            return callback(err);
+        })
+    },
+
+    getDB: function(){
+        return _db;
+    }
+}
